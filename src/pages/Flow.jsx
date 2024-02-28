c43eecc4b4dd4c1caf4eed5d57dfa4a96d5fbc86
Avinash Kumar <avinash.kumar@cognizer.ai>
2024-02-29 02:06:08 +0530

    adding basic code for react flow

---
+++ src/pages/Flow.jsx
@@ -0,0 +1,29 @@
+import ReactFlow, { Controls, Background } from "reactflow";
+import "reactflow/dist/style.css";
+
+const nodes = [
+  {
+    id: "1",
+    position: { x: 0, y: 0 },
+    data: { label: "Hello" },
+    type: "input",
+  },
+  {
+    id: "2",
+    position: { x: 100, y: 100 },
+    data: { label: "World" },
+  },
+];
+const edges = [{ id: "1-2", source: "1", target: "2" }];
+function Flow() {
+  return (
+    <div style={{ height: "800px", backgroundColor: "pink" }}>
+      <ReactFlow nodes={nodes} edges={edges}>
+        <Background />
+        <Controls />
+      </ReactFlow>
+    </div>
+  );
+}
+
+export default Flow;
