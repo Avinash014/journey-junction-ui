9981d41187f76168b6d4cb500b4c945595befef7
Avinash Kumar <avinash.kumar@cognizer.ai>
2024-02-29 02:23:10 +0530

    adding interactivity for the node

--- src/pages/Flow.jsx
+++ src/pages/Flow.jsx
@@ -1,7 +1,8 @@
 import ReactFlow, { Controls, Background } from "reactflow";
 import "reactflow/dist/style.css";
-
-const nodes = [
+import { useState, useCallback } from "react";
+import { applyEdgeChanges, applyNodeChanges, addEdge } from "reactflow";
+const initialNodes = [
   {
     id: "1",
     position: { x: 0, y: 0 },
@@ -13,12 +14,43 @@ const nodes = [
     position: { x: 100, y: 100 },
     data: { label: "World" },
   },
+  {
+    id: "3",
+    position: { x: 150, y: 150 },
+    data: { label: "hi" },
+  },
+  {
+    id: "4",
+    position: { x: 150, y: 200 },
+    data: { label: "hi" },
+  },
 ];
-const edges = [{ id: "1-2", source: "1", target: "2" }];
+const initialEdges = [{ id: "1-2", source: "1", target: "2" }];
 function Flow() {
+  const [nodes, setNodes] = useState(initialNodes);
+  const [edges, setEdges] = useState(initialEdges);
+  const onNodesChange = useCallback(
+    (changes) => setNodes((nds) => applyNodeChanges(changes, nds)),
+    []
+  );
+  const onEdgesChange = useCallback(
+    (changes) => setEdges((eds) => applyEdgeChanges(changes, eds)),
+    []
+  );
+  const onConnect = useCallback(
+    (params) => setEdges((eds) => addEdge(params, eds)),
+    []
+  );
   return (
     <div style={{ height: "800px", backgroundColor: "pink" }}>
-      <ReactFlow nodes={nodes} edges={edges}>
+      <ReactFlow
+        nodes={nodes}
+        onNodesChange={onNodesChange}
+        edges={edges}
+        onEdgesChange={onEdgesChange}
+        onConnect={onConnect}
+        fitView
+      >
         <Background />
         <Controls />
       </ReactFlow>
