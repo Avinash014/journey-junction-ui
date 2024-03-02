e5afdea04e1d38e69b934e13528f252a516123a8
Avinash Kumar <avinash.kumar@cognizer.ai>
2024-03-02 23:54:22 +0530

    updating toolbar

--- src/components/Renderer/Toolbar.jsx
+++ src/components/Renderer/Toolbar.jsx
@@ -1,6 +1,21 @@
 import React from "react";
+import SimpleButton from "../Buttons/ButtonSimple";
+import OutlineButton from "../Buttons/ButtonOutline";
 
 const Toolbar = () => {
-  return <h1>Toolbar</h1>;
+  return (
+    <div class="p-4">
+      <div class="flex flex-col">
+        <OutlineButton>Title</OutlineButton>
+        <OutlineButton>Topic</OutlineButton>
+        <OutlineButton>Sub Topic</OutlineButton>
+        <OutlineButton>Paragraph</OutlineButton>
+        <OutlineButton>Button</OutlineButton>
+        <OutlineButton>Horizontal Line</OutlineButton>
+        <OutlineButton>Vertical LIne</OutlineButton>
+        <OutlineButton>Section</OutlineButton>
+      </div>
+    </div>
+  );
 };
 export default Toolbar;
