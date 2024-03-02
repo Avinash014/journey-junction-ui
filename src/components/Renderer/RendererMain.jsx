837d7240cf81200e8fa68de048dd721a1fdbca0d
Avinash Kumar <avinash.kumar@cognizer.ai>
2024-03-03 00:23:54 +0530

    adding main renderer class

--- src/components/Renderer/RendererMain.jsx
+++ src/components/Renderer/RendererMain.jsx
@@ -1,8 +1,82 @@
-import React from "react";
-import tailwindIntegration from "@astrojs/tailwind";
-import "../../styles/global.css";
+import { useCallback } from "react";
+import ReactFlow, { ReactFlowProvider, useReactFlow } from "reactflow";
+import "reactflow/dist/style.css";
 
-const RendererMain = () => {
-  return <h1 className="text-3xl font-bold underline">renderer</h1>;
+const defaultEdges = [{ id: "ea-b", source: "a", target: "b" }];
+const defaultNodes = [
+  {
+    id: "a",
+    type: "input",
+    data: { label: "Node A" },
+    position: { x: 250, y: 25 },
+  },
+
+  {
+    id: "b",
+    data: { label: "Node B" },
+    position: { x: 100, y: 125 },
+  },
+  {
+    id: "c",
+    type: "output",
+    data: { label: "Node C" },
+    position: { x: 250, y: 250 },
+  },
+];
+
+const edgeOptions = {
+  animated: true,
+  style: {
+    stroke: "white",
+  },
 };
-export default RendererMain;
+
+const connectionLineStyle = { stroke: "white" };
+
+let nodeId = 0;
+
+function RendererMain() {
+  const reactFlowInstance = useReactFlow();
+  const onClick = useCallback(() => {
+    const id = `${++nodeId}`;
+    const newNode = {
+      id,
+      position: {
+        x: Math.random() * 500,
+        y: Math.random() * 500,
+      },
+      data: {
+        label: `Node ${id}`,
+      },
+    };
+    reactFlowInstance.addNodes(newNode);
+  }, []);
+
+  return (
+    <>
+      <div style={{ height: "800px", backgroundColor: "pink" }}>
+        <button onClick={onClick} className="btn-add">
+          add node
+        </button>
+        <ReactFlow
+          defaultNodes={defaultNodes}
+          defaultEdges={defaultEdges}
+          defaultEdgeOptions={edgeOptions}
+          fitView
+          style={{
+            backgroundColor: "#D3D2E5",
+          }}
+          connectionLineStyle={connectionLineStyle}
+        />
+      </div>
+    </>
+  );
+}
+
+export default function () {
+  return (
+    <ReactFlowProvider>
+      <RendererMain />
+    </ReactFlowProvider>
+  );
+}
