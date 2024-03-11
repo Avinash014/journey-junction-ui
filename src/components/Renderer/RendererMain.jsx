ecff4385dd30c4c94ed208d46dbc348c812e3a2a
Avinash Kumar <avinash.kumar@cognizer.ai>
2024-03-11 23:40:30 +0530

    bug fix related to state change

--- src/components/Renderer/RendererMain.jsx
+++ src/components/Renderer/RendererMain.jsx
@@ -54,26 +54,35 @@ function Flow() {
     addNode(selectedTool);
   }, []);
 
-  const addNode = (type = "output") => {
-    const id = Date.now().toString();
-    const newNode = {
-      id,
-      position: {
-        x: Math.random() * 500,
-        y: Math.random() * 500,
-        // x: 0,
-        // y: 0,
-      },
-      type: type,
-      data: {
-        label: `${type}`,
-      },
-    };
-    reactFlowInstance.addNodes(newNode);
-    setShowNodeEditor(false);
-    setCurrentNodeId(null);
-  };
+  const addNode = useCallback(
+    (type = "output") => {
+      const id = Date.now().toString();
+      const newNode = {
+        id,
+        position: {
+          x: Math.random() * 500,
+          y: Math.random() * 500,
+          // x: 0,
+          // y: 0,
+        },
+        type: type,
+        data: {
+          label: `${type}`,
+        },
+      };
+      console.log("before new node get added");
+      console.log(nodes);
+      // reactFlowInstance.addNodes(newNode);
+      // var updatedNodes = [...nodes, newNode];
+      setNodes((prevNodes) => [...prevNodes, newNode]);
+      // console.log(updatedNodes);
+      setShowNodeEditor(false);
+      setCurrentNodeId(null);
+    },
+    [nodes]
+  );
   useEffect(() => toolSelectionHandler(selectedTool), [selectedTool]);
+  useEffect(() => console.log(nodes), [nodes]);
 
   const onNodesChange = useCallback(
     (changes) => setNodes((nds) => applyNodeChanges(changes, nds)),
@@ -121,8 +130,7 @@ function Flow() {
 
 export default function () {
   return (
-    <ReactFlowProvider>
-      <Flow />
-    </ReactFlowProvider>
+    <Flow />
+    // </ReactFlowProvider>
   );
 }
